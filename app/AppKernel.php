cf0920a7dff7917103e1ccd85bc78f02c266d902
enzotrip <enzotripoli21@gmail.com>
2016-11-15 15:31:20 +0100

    Gameficator_Bundle
    Création gameficator bundle (principal)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new GR\UserBundle\GRUserBundle(),
+            new GR\GameficatorBundle\GRGameficatorBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/GR/GameficatorBundle/GRGameficatorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GR\GameficatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GRGameficatorBundle extends Bundle
+{
+}
